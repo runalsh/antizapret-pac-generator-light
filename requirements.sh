6c7c52028acf963ac90f7ae7323261b2ac43e314
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-20 23:59:09 +0300

    fix

--- requirements.sh
+++ requirements.sh
@@ -2,5 +2,5 @@ set -e
 
 sudo apt update
 sudo apt install curl coreutils gawk sipcalc python3 python3-pip -y
-sudo python3 -m pip install --upgrade pip --break-system-packages
-sudo pip3 install --upgrade dnspython --break-system-packages
+python3 -m pip install --upgrade pip
+pip3 install --upgrade dnspython
