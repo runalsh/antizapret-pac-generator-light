69fae898a8245daac7823359dfbde801d804ef0c
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-20 23:21:52 +0300

    fix exec bit

--- doall.sh
+++ doall.sh
@@ -6,12 +6,7 @@ LANG=C.UTF-8
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
-# install necessary tools
-apt update
-apt install curl coreutils gawk sipcalc python3 python3-pip -y
-python3 -m pip install --upgrade pip --break-system-packages
-pip3 install --upgrade dnspython --break-system-packages
-
+./requirements.sh
 ./parseitdog.sh # https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-dnsmasq-ipset.lst
 ./update.sh
 ./parse.sh
