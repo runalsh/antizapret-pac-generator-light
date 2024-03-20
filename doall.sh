720b0fa0e0c4c37980a3df80b3caa36617845349
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-20 22:06:46 +0300

    necessary tools

--- doall.sh
+++ doall.sh
@@ -5,6 +5,12 @@ LANG=C.UTF-8
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
+#install necessary tools
+apt update
+apt install curl coreutils gawk sipcalc python3 python3-pip -y
+python3 -m pip install --upgrade pip --break-system-packages
+pip3 install --upgrade dnspython --break-system-packages
+
 ./update.sh
 ./parse.sh
 ./process.sh
