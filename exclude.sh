8bfe415ade75b2b7a24633fdd703ecbc520ad75f
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-21 21:16:54 +0300

    disable ex

--- exclude.sh
+++ exclude.sh
@@ -1,34 +1,34 @@
-sed -i "/kino/d" temp/list.csv
-sed -i "/loud/d" temp/list.csv
-sed -i "/ront/d" temp/list.csv
-sed -i "/film/d" temp/list.csv
-sed -i "/line/d" temp/list.csv
-sed -i "/azin/d" temp/list.csv
-sed -i "/oker/d" temp/list.csv
-sed -i "/porn/d" temp/list.csv
-sed -i "/seri/d" temp/list.csv
-sed -i "/lord/d" temp/list.csv
-sed -i "/game/d" temp/list.csv
-sed -i "/vest/d" temp/list.csv
-sed -i "/slot/d" temp/list.csv
-sed -i "/play/d" temp/list.csv
-sed -i "/torr/d" temp/list.csv
-sed -i "/trad/d" temp/list.csv
-sed -i "/club/d" temp/list.csv
-sed -i "/mone/d" temp/list.csv
-sed -i "/shop/d" temp/list.csv
-sed -i "/live/d" temp/list.csv
-sed -i "/port/d" temp/list.csv
-sed -i "/rezk/d" temp/list.csv
-sed -i "/mega/d" temp/list.csv
-sed -i "/tion/d" temp/list.csv
-sed -i "/prav/d" temp/list.csv
-sed -i "/inup/d" temp/list.csv
-sed -i "/spin/d" temp/list.csv
-sed -i "/obet/d" temp/list.csv
-sed -i "/ital/d" temp/list.csv
-sed -i "/star/d" temp/list.csv
-sed -i "/gold/d" temp/list.csv
-sed -i "/seed/d" temp/list.csv
-sed -i "/plom/d" temp/list.csv
-sed -i "/utor/d" temp/list.csv
+# sed -i "/kino/d" temp/list.csv
+# sed -i "/loud/d" temp/list.csv
+# sed -i "/ront/d" temp/list.csv
+# sed -i "/film/d" temp/list.csv
+# sed -i "/line/d" temp/list.csv
+# sed -i "/azin/d" temp/list.csv
+# sed -i "/oker/d" temp/list.csv
+# sed -i "/porn/d" temp/list.csv
+# sed -i "/seri/d" temp/list.csv
+# sed -i "/lord/d" temp/list.csv
+# sed -i "/game/d" temp/list.csv
+# sed -i "/vest/d" temp/list.csv
+# sed -i "/slot/d" temp/list.csv
+# sed -i "/play/d" temp/list.csv
+# sed -i "/torr/d" temp/list.csv
+# sed -i "/trad/d" temp/list.csv
+# sed -i "/club/d" temp/list.csv
+# sed -i "/mone/d" temp/list.csv
+# sed -i "/shop/d" temp/list.csv
+# sed -i "/live/d" temp/list.csv
+# sed -i "/port/d" temp/list.csv
+# sed -i "/rezk/d" temp/list.csv
+# sed -i "/mega/d" temp/list.csv
+# sed -i "/tion/d" temp/list.csv
+# sed -i "/prav/d" temp/list.csv
+# sed -i "/inup/d" temp/list.csv
+# sed -i "/spin/d" temp/list.csv
+# sed -i "/obet/d" temp/list.csv
+# sed -i "/ital/d" temp/list.csv
+# sed -i "/star/d" temp/list.csv
+# sed -i "/gold/d" temp/list.csv
+# sed -i "/seed/d" temp/list.csv
+# sed -i "/plom/d" temp/list.csv
+# sed -i "/utor/d" temp/list.csv
