2713cdbe49f623aca03730808d8ea61c89817cc6
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-23 09:13:02 +0300

    impovement

---
+++ svintuss.sh
@@ -0,0 +1,10 @@
+set -e
+
+rm -rf svintuss-*.lst
+wget -O svintuss-`date '+%d-%B-%Y'`.lst https://raw.githubusercontent.com/svintuss/unblock/main/unblock.txt
+sed -i '/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/d' svintuss-`date '+%d-%B-%Y'`.lst
+cat svintuss-`date '+%d-%B-%Y'`.lst >> ./config/include-hosts-custom.txt
+rm -rf svintuss-*.lst
+awk '! a[$0]++' ./config/include-hosts-custom.txt
+# sort -o ./config/include-hosts-custom.txt ./config/include-hosts-custom.txt
+sed -i '/^$/d' ./config/include-hosts-custom.txt
