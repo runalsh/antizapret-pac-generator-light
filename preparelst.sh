14bb0990c1e7bad589a84faf0c8ce9bf7555c5a5
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-03-23 09:27:05 +0300

    impr5

--- preparelst.sh
+++ preparelst.sh
@@ -1,10 +1,7 @@
-sed -i '/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/d' ./config/include-hosts-custom.txt
-sed -i '/[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}/d' ./config/include-hosts-custom.txt
+sed -i '/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
+sed -i '/[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
 
-awk '! a[$0]++' ./config/include-hosts-custom.txt
+awk '! a[$0]++' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
 # sort -o ./config/include-hosts-custom.txt ./config/include-hosts-custom.txt
-sed -i '/^$/d' ./config/include-hosts-custom.txt
-sed -i '/#/d' ./config/include-hosts-custom.txt
-
-awk '! a[$0]++' ./config/exclude-hosts-custom.txt
-sed -i '/#/d' ./config/exclude-hosts-custom.txt
+sed -i '/^$/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
+sed -i '/#/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
