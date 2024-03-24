c20f1f074bd4fb9254d216a5152631e8fd850a0d
runalsh <21105673+runalsh@users.noreply.github.com>
2024-03-24 13:52:10 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,10 @@
 set -e
 
 sudo apt update
-sudo apt install curl coreutils gawk sipcalc python3 python3-pip idn -y
+sudo apt install curl coreutils gawk sipcalc python3 python3-pip idn pyhon3-dnspython -y
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
 python3 -m pip install --upgrade pip
-pip3 install --upgrade dnspython
+# pip3 install --upgrade dnspython
 # python3 -m pip install --upgrade pip --break-system-packages
 # pip3 install --upgrade dnspython --break-system-packages
 # sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
