5148cc53992408811537ce9684e6da762d421c6d
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-04-12 10:21:11 +0300

    add dnsmasq gen

--- process.sh
+++ process.sh
@@ -18,4 +18,16 @@ set -e
 #cp result/iplist_all.txt /etc/squid/whitelistedips.txt
 #systemctl reload squid || true
 
+#openwrt set
+# rm -rf /etc/dnsmasq.d/antizapret-openwrt.conf
+# ipset flush vpn_ipsum
+# mkdir -p /etc/dnsmasq.d/
+# cp result/dnsmasq-ipset.conf /etc/dnsmasq.d/antizapret-openwrt.conf
+# # i have no idea why or how, but this command makes it all work
+# iptables -I PREROUTING -t mangle -m set --match-set vpn_ipsum dst -j MARK --set-mark 1
+# /etc/init.d/dnsmasq restart
+# /etc/init.d/firewall restart
+# iptables -I PREROUTING -t mangle -m set --match-set vpn_ipsum dst -j MARK --set-mark 1
+# echo "\"no lease, failing\" error is normal."
+
 exit 0
