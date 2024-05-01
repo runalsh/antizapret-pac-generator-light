ca07288bdbdbd403eccf6a20f014f42dd1b68a70
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-05-01 12:10:03 +0300

    add antifilter ipsum

--- antifilter-ce.sh
+++ antifilter-ce.sh
@@ -3,6 +3,8 @@ set -e
 rm -rf antifilter-*.lst
 wget -O antifilter-`date '+%d-%B-%Y'`.lst https://community.antifilter.download/list/domains.lst
 cat antifilter-`date '+%d-%B-%Y'`.lst >> ./config/include-hosts-custom.txt
+wget -O antifilter-ip-`date '+%d-%B-%Y'`.lst https://antifilter.download/list/ipsum.lst
+cat antifilter-ip-`date '+%d-%B-%Y'`.lst >> ./config/include-ips-custom.txt
 rm -rf antifilter-*.lst
 # awk '! a[$0]++' ./config/include-hosts-custom.txt
 # # sort -o ./config/include-hosts-custom.txt ./config/include-hosts-custom.txt
