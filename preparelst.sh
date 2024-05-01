2c791071c376d368b09c4d88eb1f85dc2a10f311
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-05-01 12:49:07 +0300

    change in lists

--- preparelst.sh
+++ preparelst.sh
@@ -1,7 +1,7 @@
 sed -i '/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
 sed -i '/[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}\:[0-9]\{1,4\}/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
 
-awk '! a[$0]++' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
+awk '! a[$0]++' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt,include-ips-custom.txt,exclude-ips-custom.txt}
 # sort -o ./config/include-hosts-custom.txt ./config/include-hosts-custom.txt
 sed -i '/^$/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
 sed -i '/#/d' ./config/{include-hosts-custom.txt,exclude-hosts-custom.txt}
