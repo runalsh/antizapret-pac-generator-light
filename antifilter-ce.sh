59161429cc0a069c8195453c0e5481e706e0957f
runalsh <9218922-runalsh@users.noreply.gitlab.com>
2024-05-01 14:54:14 +0300

    test

--- antifilter-ce.sh
+++ antifilter-ce.sh
@@ -5,6 +5,8 @@ wget -O antifilter-`date '+%d-%B-%Y'`.lst https://community.antifilter.download/
 cat antifilter-`date '+%d-%B-%Y'`.lst >> ./config/include-hosts-custom.txt
 wget -O antifilter-ip-`date '+%d-%B-%Y'`.lst https://antifilter.download/list/ip.lst
 cat antifilter-ip-`date '+%d-%B-%Y'`.lst >> ./config/include-ips-custom.txt
+wget -O antifilter-ip-resolve-`date '+%d-%B-%Y'`.lst https://antifilter.download/list/ipresolve.lst
+cat antifilter-ip-resolve-`date '+%d-%B-%Y'`.lst >> ./config/include-ips-custom.txt
 rm -rf antifilter-*.lst
 # awk '! a[$0]++' ./config/include-hosts-custom.txt
 # # sort -o ./config/include-hosts-custom.txt ./config/include-hosts-custom.txt
