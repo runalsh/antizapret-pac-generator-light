5d0bda263b9e126c95491ec5a90768a675924cd6
runalsh <21105673+runalsh@users.noreply.github.com>
2024-05-30 02:20:10 +0300

    upd ips and hosts

--- otherlist.sh
+++ otherlist.sh
@@ -3,8 +3,8 @@ set -e
 rm -rf other-*.lst
 
 # spotify
-wget -O other-spotify-`date '+%d-%B-%Y'`.lst https://raw.githubusercontent.com/naruto522ru/ipranges/main/spotify/domains.txt
-cat other-spotify-`date '+%d-%B-%Y'`.lst >> ./config/include-hosts-custom.txt
+wget -O other-spotify-`date '+%d-%B-%Y'`.lst https://raw.githubusercontent.com/naruto522ru/ipranges/main/spotify/ipv4.txt
+cat other-spotify-`date '+%d-%B-%Y'`.lst >> ./config/include-ips-custom.txt
 #facebook
 #https://raw.githubusercontent.com/antonme/ipnames/master/ext-dns-facebook.txt
 #https://raw.githubusercontent.com/antonme/ipnames/master/dns-facebook.txt
